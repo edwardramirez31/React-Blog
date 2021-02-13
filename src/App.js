aa1a112055d89714c5b71c71c21a92ebfc630d55
Edward Ramirez <edal_ramirez@hotmail.com>
2021-02-13 14:23:38 -0500

    Creating and importing two new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return ( 
+    <navbar className="navbar">
+      <h1>My React Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </navbar>
+
+   );
+}
+ 
+export default Navbar;
