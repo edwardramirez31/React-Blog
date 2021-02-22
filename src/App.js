e266f42a3fcc61372a18a022e67f889525d952fb
Edward <edal_ramirez@hotmail.com>
2021-02-21 21:23:09 -0500

    Creating React Routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import Home from './Home';
 import Navbar from './Navbar';
 import Counter from './Counter';
+import Create from './Create';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path='/counter'>
+              <Counter buttonText="Add one">
+                <p>This is a child prop</p>
+              </Counter>
+            </Route>
+            <Route path='/create'>
+              <Create />
+            </Route>
+            <Route exact path='/create/ward'>
+              <h2>Create Ward</h2>
+            </Route>
+            <Route path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-      <Counter buttonText="Add one">
-        <p>This is a child prop</p>
-      </Counter>
-
-    </div>
+    </Router>
   );
 }
 
